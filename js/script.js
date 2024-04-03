357df60c59ac84e73f1ac43bec65265be2d850ef
2JD <2jd@bglab.local>
2024-04-03 10:34:19 -0300

    salvar tudo

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const submit = document.querySelector('#jogar')
 const jogada = document.querySelector('#txtNumero')
 const jogadasAnteriores = document.querySelector('.vezes')
 const jogadasRestantes = document.querySelector('.numChances')
-const recomecar = document.querySelector('.resultados')
+const recomecar = document.querySelector('.resultado')
 const avisos = document.querySelector('.avisos')
 
 const p = document.createElement('p')
@@ -35,6 +35,11 @@ function validaChances(tentativa) {
     }
     else if(tentativa < 1 || tentativa > 100) {
         alert('coloque um numêro entre 1 e 100')
+        jogada.value = ''
+        jogada.focus()
+    }
+    else if(numerosJogados.includes(tentativa)) {
+        alert('atenção, tu já colocou esse número')
         jogada.value = ''
         jogada.focus()
     }
@@ -75,4 +80,15 @@ function displayTentativas(tentativa) {
 
 function msg(texto) {
     avisos.innerHTML =  `<h1>${texto}</h1>`
+}
+
+function fimJogo() {
+    jogada.value = ''
+    jogada.setAttribute('disabled','')
+    submit.setAttribute('disabled','')
+    p.classList.add('button')
+    p.innerHTML = `<h1 id="iniciarJogada">Iniciar Jogo</h1>`
+    recomecar.appendChild(p)
+    playGame = false
+    iniciarJogo()
 }
